2dbf5f28ef6726c019ee163df730e6b548b2f7ff
guilhermebodin <guilherme.b.moraes@gmail.com>
2023-09-27 16:50:31 -0300

    fix sqlite write

--- src/sqlite.jl
+++ src/sqlite.jl
@@ -6,9 +6,11 @@ function sqlite_write_2d_one_shot(file_name::String, arr::Matrix{Float32}, agent
     for i in 1:n_agents
         df[!, agent_names[i]] = arr[:, i]
     end
-        
+    
+    rm(file_name*".sqlite3", force = true)
     db = SQLite.DB(file_name*".sqlite3")
     SQLite.load!(df, db, file_name)
+    DBInterface.close!(db)
 
     return nothing
 end
