7c816a336ce56d3ee5d3e5df14cf48cc666b43ca
guilhermebodin <guilherme.b.moraes@gmail.com>
2023-09-29 02:24:39 -0300

    version working

--- src/arrow.jl
+++ src/arrow.jl
@@ -1,4 +1,3 @@
-# TODO maybe this should be a struct
 # TODO add compression here
 mutable struct QuiverWriter
     arrow_writer::Arrow.Writer
@@ -6,57 +5,63 @@ mutable struct QuiverWriter
     agents::Vector{Symbol}
     initial_date::Union{Nothing, Dates.DateTime}
     stage_type::Union{Nothing, String}
-end
-
-function create_file(
-    filename::String,
-    dimensions::Vector{String},
-    agents::Vector{String};
-    initial_date::Union{Nothing, Dates.DateTime} = nothing,
-    stage_type::Union{Nothing, String} = nothing,
-    remove_if_exists::Bool = true
-)
-    # Make a gc pass before starting
-    Base.GC.gc()
-
-    if isfile(filename)
-        if remove_if_exists
-            rm(filename; force = true)
-        else
-            error("File $filename already exists.")
-        end
-    end
-
-    empty_df_quiver = DataFrames.DataFrame()
-    for dim in dimensions
-        empty_df_quiver[!, Symbol(dim)] = Int32[]
-    end
-    for agent in agents
-        empty_df_quiver[!, Symbol(agent)] = Float32[]
-    end
-
-    metadata = Dict{String, String}()
-    if stage_type !== nothing
-        metadata["stage_type"] = stage_type
-    end
-    if initial_date !== nothing
-        metadata["initial_date"] = string(initial_date)
-    end
-    metadata["dimension_columns"] = string(length(dimensions))
-    metadata["agents_columns"] = string(length(agents))
-
-    arrow_writer = open(Arrow.Writer, filename)
-    Arrow.write(arrow_writer, empty_df_quiver)
-
-    writer = QuiverWriter(
-        arrow_writer, 
-        Symbol.(dimensions), 
-        Symbol.(agents), 
-        initial_date, 
-        stage_type
+    last_dimension_added::Vector{Int32}
+    function QuiverWriter(
+        filename::String,
+        dimensions::Vector{String},
+        agents::Vector{String};
+        initial_date::Union{Nothing, Dates.DateTime} = nothing,
+        stage_type::Union{Nothing, String} = nothing,
+        remove_if_exists::Bool = true
+        # TODO 
+        # add version and unit
+        # add compression, if we turn off is much faster to read (it appears that reading is a bottleneck when compressed)
     )
+        # Make a gc pass before starting, not really needed
+        # TODO remove
+        Base.GC.gc()
 
-    return writer
+        # TODO put inside a function
+        if isfile(filename)
+            if remove_if_exists
+                rm(filename; force = true)
+            else
+                error("File $filename already exists.")
+            end
+        end
+
+        # TODO put inside a function
+        empty_df_quiver = DataFrames.DataFrame()
+        for dim in dimensions
+            empty_df_quiver[!, Symbol(dim)] = Int32[]
+        end
+        for agent in agents
+            empty_df_quiver[!, Symbol(agent)] = Float32[]
+        end
+
+        # TODO put inside a function
+        metadata = Dict{String, String}()
+        if stage_type !== nothing
+            metadata["stage_type"] = stage_type
+        end
+        if initial_date !== nothing
+            metadata["initial_date"] = string(initial_date)
+        end
+        metadata["dimension_columns"] = string(length(dimensions))
+        metadata["agents_columns"] = string(length(agents))
+
+        arrow_writer = open(Arrow.Writer, filename; metadata = metadata)
+        Arrow.write(arrow_writer, empty_df_quiver)
+
+        return new(
+            arrow_writer, 
+            Symbol.(dimensions), 
+            Symbol.(agents), 
+            initial_date, 
+            stage_type,
+            fill(Int32(-10000), length(dimensions))
+        )
+    end
 end
 
 function write!(writer::QuiverWriter, df::DataFrames.DataFrame)
@@ -65,12 +70,50 @@ function write!(writer::QuiverWriter, df::DataFrames.DataFrame)
 end
 
 function write!(writer::QuiverWriter, dimensions::Matrix{Int32}, agents::Matrix{Float32})
+    if !dimensions_are_compliant(writer, dimensions)
+        error("Dimensions are not in order.")
+    end
     intermediary_df = DataFrames.DataFrame(agents, writer.agents)
     for dim in 1:length(writer.dimensions)
         DataFrames.insertcols!(intermediary_df, dim, writer.dimensions[dim] => dimensions[:, dim])
     end
     Quiver.write!(writer, intermediary_df)
+    writer.last_dimension_added = dimensions[end, :]
     return nothing
+end
+
+# This is something for safety but could be turn off
+# It helps us guarantee that the dimensions follow their expected behaviour
+# There should be smarter ways of doing these checks
+function dimensions_are_compliant(writer::QuiverWriter, dimensions::Matrix{Int32})::Bool
+    # The first dimension must be grater or equal than the last one added
+    first_dim = dimensions[1, :]
+    for (i, dim) in enumerate(first_dim)
+        if dim > writer.last_dimension_added[i]
+            # If this is true then everything the order is certainly respected
+            break
+        elseif dim == writer.last_dimension_added[i]
+            # If this is true we still need to check if the next dimension respects the order
+            continue
+        else # dim < writer.last_dimension_added[i]
+           return false
+        end
+    end
+
+    # The next element of dimensions must be grater or equal than the previous one
+    for i in 1:size(dimensions, 1) - 1
+        for dim in axes(dimensions, 2)
+            if dimensions[i + 1, dim] > dimensions[i, dim]
+                break
+            elseif dimensions[i + 1, dim] == dimensions[i, dim]
+                continue
+            else
+                return false
+            end
+        end
+    end 
+
+    return true
 end
 
 function close!(writer::QuiverWriter)
@@ -78,7 +121,79 @@ function close!(writer::QuiverWriter)
     return nothing
 end
 
-function columns(filename::String)
+# Reader
+mutable struct QuiverReader
+    df::DataFrames.DataFrame
+    dimensions::Vector{Symbol}
+    agents_to_read::Vector{Symbol}
+    metadata::Dict{String, String}
+
+    function QuiverReader(filename::String; 
+        agents::Union{Nothing, Vector{Symbol}} = nothing,    
+    )
+        Quiver.assert_file_exists(filename)
+        tbl = Arrow.Table(filename)
+        df = DataFrames.DataFrame(tbl)
+        metadata = Arrow.getmetadata(tbl)
+        cols = Arrow.names(tbl)
+        dimensions_cols = parse(Int, metadata["dimension_columns"])
+        dimensions = cols[1:dimensions_cols]
+
+        agents_to_read = if agents === nothing
+            cols[dimensions_cols + 1:end]
+        else
+            agents
+        end
+
+        return new(
+            df,
+            dimensions,
+            agents_to_read,
+            metadata
+        )
+    end
+end
+
+# TODO maybe find a way of making this a kwargs... instead of a NamedTuple
+# This is purely for style, instead of searching for Quiver.read(reader, (;stage = 231, scenario = 1))
+# search for Quiver.read(reader; stage = 231, scenario = 1)
+function read(reader::QuiverReader, dimensions_to_query::NamedTuple)
+    assert_dimensions_are_in_order(reader, dimensions_to_query)
+    indexes_to_search_at_dimension = Vector{UnitRange{Int}}(undef, length(dimensions_to_query) + 1)
+    indexes_found_at_dimension = Vector{UnitRange{Int}}(undef, length(dimensions_to_query))
+    indexes_to_search_at_dimension[1] = 1:size(reader.df, 1)
+    for (i, dim_to_query) in enumerate(dimensions_to_query)
+        # The searchsorted function makes a binary search on the indexes_to_search
+        # TODO this needs better explanation. It is a series of chained searches
+        # NOTE: The first one is separated from the others because it allocates a lot, 
+        # acessing  DataFrame via df[!, dim] is much faster than df[indexes, dim] because it does not allocate
+        if i == 1
+            indexes_found_at_dimension[i] = searchsorted(reader.df[!, reader.dimensions[i]], dim_to_query)
+            indexes_to_search_at_dimension[i + 1] = indexes_found_at_dimension[i]
+        else
+            indexes_found_at_dimension[i] = searchsorted(reader.df[indexes_to_search_at_dimension[i], reader.dimensions[i]], dim_to_query)
+            indexes_to_search_at_dimension[i + 1] = indexes_found_at_dimension[i] .+ indexes_to_search_at_dimension[i][1] .- 1
+        end
+
+        if isempty(indexes_found_at_dimension[i])
+            error("Dimension $dimensions_to_query not found.")
+        end
+    end
+    return reader.df[indexes_to_search_at_dimension[end], :]
+end
+
+function assert_dimensions_are_in_order(reader::QuiverReader, dimensions_to_query::NamedTuple)
+    keys_dims_to_query = keys(dimensions_to_query)
+    for (i, dim) in enumerate(keys_dims_to_query)
+        if dim != reader.dimensions[i]
+            error("Dimensions must be read in the order of the file. (Expected the order $(reader.dimensions)")
+        end
+    end
+    return nothing
+end
+
+# utils
+function names(filename::String)
     assert_file_exists(filename)
     return filename |> Arrow.Table |> Arrow.names
 end
@@ -101,9 +216,4 @@ end
 @inline function assert_file_exists(filename::String)
     @assert isfile(filename) "File $filename does not exist."
     return nothing
-end
-
-function read_dimensions(quiver_file::String, dims...)
-    tbl = read_table_from_file!(quiver_file)
-    return Arrow.getcolumn(tbl, dims...)
 end
--- test/script.jl
+++ test/script.jl
@@ -2,26 +2,69 @@ using Quiver
 using Dates
 using DataFrames
 using Arrow
+using Random
 
-filename = "test7.arrow"
-dimensions = ["stage", "scenario"]
-agents = ["agent_$i" for i in 1:500]
-writer = Quiver.create_file(filename, dimensions, agents; initial_date = DateTime(2006, 1, 1), stage_type = "month")
+# just an example of a commonly big file
+initial_date = DateTime(2006, 1, 1)
+num_stages = 60
+dates = collect(initial_date:Dates.Month(1):initial_date + Dates.Month(num_stages - 1))
+num_scenarios = 1200
+num_blocks_per_stage = Int32.(Dates.daysinmonth.(dates) .* 24)
+num_agents = 20
+# more or less the size of this file
+total_size_gb = 4 * num_stages * num_scenarios * maximum(num_blocks_per_stage) * (num_agents + 3 #= from the dimension columns =#) / 1e9
 
-dimensions = [collect(Int32, 1:1000) repeat(Int32[1], 1000)] 
-@time for _ in 1:2000
-    agents = rand(Float32, 1000, 500)
+filename = "test_5gb_variable_block_rand.arrow"
+# This should be a vector of symbols
+dimensions = ["stage", "scenario", "block"]
+agents = ["agent_$i" for i in 1:num_agents]
+writer = Quiver.QuiverWriter(filename, dimensions, agents; initial_date = initial_date, stage_type = "month")
+
+# I am allocating everything before so it does not count on the @time
+
+@time for stage in 1:num_stages
+    println("$stage")
+    i = 1
+    agents = rand(Float32, num_scenarios * num_blocks_per_stage[stage], num_agents)
+    size_of_batch_write_in_mb = sizeof(agents) / 1e6
+    dimensions = Matrix{Int32}(undef, num_scenarios * num_blocks_per_stage[stage], 3)
+    for scenario in 1:num_scenarios
+        for block in 1:num_blocks_per_stage[stage]
+            dimensions[i, 1] = stage
+            dimensions[i, 2] = scenario
+            dimensions[i, 3] = block
+            # This is will create an invalid dimension
+            # uncomment this part and it will throw an error
+            # if stage == 48
+            #     dimensions[i, 1] = 1
+            # end
+            i += 1
+        end
+    end
     Quiver.write!(writer, dimensions, agents)
 end
 Quiver.close!(writer)
 
-df = Quiver.dataframe("test7.arrow")
-s = df |> Matrix |> sizeof
-s / 1e6
+@time reader = Quiver.QuiverReader(filename);
 
-stream = Arrow.Stream(filename)
+# We can query the stage, scenario, block on a random order
+# right now it returns a dataframe but it could be a matrix
+@time df = Quiver.read(reader, (;stage = 43, scenario = 389, block = 23))
+@time df = Quiver.read(reader, (;stage = 22, scenario = 1190, block = 23))
 
-p = 1
-for table in stream
-    p += 1
-end
+# We can query a stage and scenario and it will return every block from that stage, scenario
+@time df = Quiver.read(reader, (;stage = 31, scenario = 234))
+
+# We can query a stage and it will return every scenario and block from that stage
+@time df = Quiver.read(reader, (;stage = 21))
+
+# We can query the metadata and other things
+Quiver.metadata(filename)
+Quiver.schema(filename)
+Quiver.names(filename)
+
+### notes
+# This should work with negative stages, just haven't tested it yet
+# The implementation might not be the best but it is reasonably easy to understand
+# It also allows for holes in the data, so if you have a stage 1 and a stage 3 but not a stage 2 it will work. but I haven't tested it yet
+# It allows for a variable number of anything, stages, scenarios, blocks, whatever. You can also create your own dimensions as you wish
