8fa19fc932c2e95cb6ba1d24dfe713cc2fceb4a9
guilhermebodin <guilherme.b.moraes@gmail.com>
2024-08-22 13:56:32 -0300

    minimally working

---
+++ test/test_graf_extension.jl
@@ -0,0 +1,92 @@
+module TestGrafExtension
+
+using Dates
+using PSRClassesInterface
+using Quiver
+using Test
+
+PSRI = PSRClassesInterface
+
+function test_graf_convertion_fixed_blocks()
+    BLOCKS = 3
+    SCENARIOS = 5
+    STAGES = 12
+    INITIAL_STAGE = 4
+
+    FILE_PATH = joinpath(@__DIR__, "test_convert")
+
+    for impl in [Quiver.binary]
+        for stage_type in [PSRI.STAGE_MONTH] #, PSRI.STAGE_WEEK, PSRI.STAGE_DAY]
+            iow = PSRClassesInterface.open(
+                PSRClassesInterface.OpenBinary.Writer,
+                FILE_PATH;
+                blocks = BLOCKS,
+                scenarios = SCENARIOS,
+                stages = STAGES,
+                agents = ["X", "Y", "Z"],
+                unit = "MW",
+                # optional:
+                initial_stage = INITIAL_STAGE,
+                initial_year = 2020,
+                stage_type = stage_type,
+            )
+
+            for t in 1:STAGES, s in 1:SCENARIOS, b in 1:BLOCKS
+                X = t + s + 0.0
+                Y = s - t + 0.0
+                Z = t + s + b * 100.0
+                PSRClassesInterface.write_registry(iow, [X, Y, Z], t, s, b)
+            end
+
+            PSRClassesInterface.close(iow)
+
+            Quiver.convert(
+                FILE_PATH,
+                PSRClassesInterface.OpenBinary.Reader,
+                impl
+            )
+
+            # @test isfile(FILE_PATH * "toml")
+            # @test isfile(FILE_PATH * "quiv") || isfile(FILE_PATH * "quiv.csv")
+
+            # Test if data was correctly converted
+            reader = Quiver.Reader{impl}(FILE_PATH)
+            num_stages = reader.metadata.dimension_size[1]
+            num_scenarios = reader.metadata.dimension_size[2]
+            num_blocks = reader.metadata.dimension_size[3]
+            for t in 1:num_stages
+                for s in 1:num_scenarios
+                    for b in 1:num_blocks
+                        X = t + s + 0.0
+                        Y = s - t + 0.0
+                        Z = t + s + b * 100.0
+                        Quiver.goto!(reader; stage = t, scenario = s, block = b)
+                        @test reader.data == [X, Y, Z]
+                    end
+                end
+            end
+
+            @test reader.metadata.labels == ["X", "Y", "Z"]
+            @test reader.metadata.unit == "MW"
+            @test reader.metadata.initial_date == DateTime(2020, 4, 1)
+            @test reader.metadata.time_dimension == :stage
+            @test reader.metadata.dimensions == [:stage, :scenario, :block]
+        end
+    end
+end
+
+function runtests()
+    Base.GC.gc()
+    Base.GC.gc()
+    for name in names(@__MODULE__; all = true)
+        if startswith("$name", "test_")
+            @testset "$(name)" begin
+                getfield(@__MODULE__, name)()
+            end
+        end
+    end
+end
+
+TestGrafExtension.runtests()
+
+end
