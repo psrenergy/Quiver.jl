604fa923bfa6e041e81a1ba4670f94728417a7c7
Mateus <mateus.kaiss@ufpr.br>
2024-09-25 17:30:22 -0300

    mk/ testing changes in make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -9,18 +9,23 @@ using Documenter
 
 makedocs(;
     modules = [Quiver],
-    doctest = false,
+    doctest = true,
     clean = true,
-    format = Documenter.HTML(;
-        mathengine = Documenter.MathJax2(),
-        prettyurls = false,
-        canonical = "https://documenter.quiver.juliadocs.org/stable/",
-        # Prevents the edit on github button from showing up
-        edit_link = nothing,
-        footer = nothing,
-        disable_git = true,
-        repolink = nothing,
-    ),
+    format = Documenter.HTML(; mathengine = Documenter.MathJax2()),
+    sitename = "PSRClassesInterface.jl",
+    authors = "psrenergy",
+    # format = Documenter.HTML(;
+    #     mathengine = Documenter.MathJax2(),
+    #     prettyurls = false,
+    #     # canonical = "https://documenter.quiver.juliadocs.org/stable/",
+    #     sitename = "PSRClassesInterface.jl",
+    #     authors = "psrenergy",
+    #     # Prevents the edit on github button from showing up
+    #     # edit_link = nothing,
+    #     # footer = nothing,
+    #     # disable_git = true,
+    #     # repolink = nothing,
+    # ),
     sitename = "Quiver.jl",
     warnonly = true,
     pages = [
