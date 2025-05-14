516d53c496c8bd89d7226fb4b7d8e7521a7a8704
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-05-14 10:45:46 -0300

    Project updates

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,20 +1,9 @@
-using Aqua
-using Quiver
-using Test
-
 function test_modules(dir::AbstractString)
     result = String[]
     for (root, dirs, files) in walkdir(dir)
         append!(result, filter!(f -> occursin(r"test_(.)+\.jl", f), joinpath.(root, files)))
     end
     return result
-end
-
-@testset "Aqua" begin
-    @testset "Ambiguities" begin
-        Aqua.test_ambiguities(Quiver, recursive = false)
-    end
-    Aqua.test_all(Quiver, ambiguities = false)
 end
 
 for file in test_modules(@__DIR__)
