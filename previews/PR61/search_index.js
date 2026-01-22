b0023f860899f91e1053e388fff58cdc0bdc9114
Documenter.jl <documenter@juliadocs.github.io>
2026-01-22 22:29:33 +0000

    build based on 18a87c6

---
+++ previews/PR61/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Quiver.jl","page":"Home","title":"Quiver.jl","text":"","category":"section"},{"location":"#Installation","page":"Home","title":"Installation","text":"pkg> add Quiver","category":"section"}]
+}
